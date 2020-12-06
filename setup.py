dde13e058b29ec9f7450f987cc1a7d27fac35a9d
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-12-06 13:40:23 +0200

    Mark aiosignal as Python3-only package (#165)

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ args = dict(
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
