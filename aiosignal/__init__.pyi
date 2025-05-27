5dcd998228fc48fe7bc8168f98d3c8773e1dc1dd
Vizonex <114684698+Vizonex@users.noreply.github.com>
2025-05-27 08:42:16 -0500

    Add Type hints to stubfile

--- aiosignal/__init__.pyi
+++ aiosignal/__init__.pyi
@@ -1,12 +1,13 @@
 from typing import Any, Generic, TypeVar
 
 from frozenlist import FrozenList
+from .typedefs import P, AsyncFunc
 
 __all__ = ("Signal",)
 
 _T = TypeVar("_T")
 
-class Signal(FrozenList[_T], Generic[_T]):
+class Signal(FrozenList[AsyncFunc[P, _T]], Generic[_T]):
     def __init__(self, owner: Any) -> None: ...
     def __repr__(self) -> str: ...
-    async def send(self, *args: Any, **kwargs: Any) -> None: ...
+    async def send(self, *args: P.args, **kwargs: P.kwargs) -> None: ...
