bfa882b4b01632c12376ca4987f7bf20609d7c3e
Vizonex <114684698+Vizonex@users.noreply.github.com>
2025-06-03 16:36:03 -0500

    Add 2 new methods for defining function Parameters to a Signal
    Also added documentation on how the 2 new methods are to be utilized...

--- aiosignal/__init__.py
+++ aiosignal/__init__.py
@@ -1,20 +1,25 @@
 import sys
-from typing import Awaitable, Callable, TypeVar
-
+from typing import Awaitable, Callable, TypeVar, Type
+from abc import ABCMeta
 from frozenlist import FrozenList
 
 if sys.version_info >= (3, 10):
-    from typing import ParamSpec
+    from typing import ParamSpec, Concatenate
 else:
-    from typing_extensions import ParamSpec
+    from typing_extensions import ParamSpec, Concatenate
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
 
 _P = ParamSpec("_P")
-_T = TypeVar("_T")
+_T = TypeVar("_T", contravariant=True)
 _AsyncFunc = Callable[_P, Awaitable[_T]]
 
 __version__ = "1.3.2"
 
-__all__ = ("Signal",)
+__all__ = ("Signal", "signal_func", "signal_method")
 
 
 class Signal(FrozenList[_AsyncFunc[_P, _T]]):
@@ -51,3 +56,58 @@ class Signal(FrozenList[_AsyncFunc[_P, _T]]):
         """Decorator to add a function to this Signal."""
         self.append(func)
         return func
+
+
+def signal_func(_: _AsyncFunc[_P, None]) -> type[Signal[_P, None]]:
+    """overrides a function to define a function as a 
+    signal that can be defined with typehinted parameters::
+
+        from aiosignal import signal_func
+
+        @signal_func
+        async def my_signal(a:int, b:int) -> None:...
+
+        # my_signal has been transformed to a Signal 
+        # and it should now be typehinted as: 
+        # (owner: object) -> Signal[(a: int, b: int), None]
+         
+        event = my_signal(None)
+
+        # Now we can create helpuful callbacks with 
+        # helpful parameters to help us if were stuck...
+
+        @event
+        async def on_my_event(a:int, b:int):
+            ...
+
+    """
+    return Signal
+
+
+def signal_method(_: _AsyncFunc[Concatenate[Self, _P], None]) -> type[Signal[_P, None]]:
+    """Helper that typehints a class method as a signal
+    This could help assist in creating Protocol Types that can 
+    define the creation of an object 
+
+    ::
+
+        from typing import TypeVar, Protocol
+
+        A = TypeVar("A")
+        B = TypeVar("B")
+
+        class MySignalProtocol(Protocol[A, B]):
+            @signal_method
+            def __call__(self, a: A, b: B) -> None:...
+
+        MySignal: MySignalProtocol[dict, int] = Signal
+
+        # Signal is now been typehinted via protocol
+        # pyright or other ides should be able to 
+        # identify it as: 
+        #   (owner:object) -> Signal[(a: dict, b: int), None]
+
+        signal = MySignal()
+
+    """
+    return Signal
